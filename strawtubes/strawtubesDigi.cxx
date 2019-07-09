aaa9eb000254846350e3e84067a93580800fa1a3
Andrew Zelenov <andrew.zelenov@aol.com>
2019-07-09 16:46:13 +0200

    checking bugs

--- strawtubes/strawtubesDigi.cxx
+++ strawtubes/strawtubesDigi.cxx
@@ -24,7 +24,7 @@ Double_t strawtubesDigi::f2calculation() {
 void strawtubesDigi::driftTimeCalculation() {
    mpvTime = timeDependence->Eval(dist2Wire);
    LandauSigma = mpvTime * f2calculation() / 100;
-   driftTime = rand->Landau(mpvTime, LandauSigma);
+   driftTime = mpvTime;
 }
 
 void strawtubesDigi::recoDistCalculation(Double_t time) {
