82780bf550f76fc6d29d0e8484fb478aa538f3f6
Andrew Zelenov <andrew.zelenov@aol.com>
2019-07-09 16:44:14 +0200

    checking bugs

--- python/shipDigiReco.py
+++ python/shipDigiReco.py
@@ -782,7 +782,7 @@ class ShipDigiReco:
      SmearedHits.append( {'digiHit':key,'xtop':stop.x(),'ytop':stop.y(),'z':stop.z(),'xbot':start.x(),'ybot':start.y(),'dist':smear, 'detID':detID} )
      # Note: top.z()==bot.z() unless misaligned, so only add key 'z' to smearedHit
 
-     h['vshape'].Fill(strawtubesDigi.getDist2Wire(), driftTime)
+     h['vshape'].Fill(p.dist2Wire(), driftTime)
 
 
      if abs(stop.y())==abs(start.y()): h['disty'].Fill(smear)
