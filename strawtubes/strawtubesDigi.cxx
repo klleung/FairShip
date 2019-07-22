5630da171ecccaec6e08c997178affe646900edf
Andrew Zelenov <andrew.zelenov@aol.com>
2019-07-22 18:32:15 +0200

    updating methods

--- strawtubes/strawtubesDigi.cxx
+++ strawtubes/strawtubesDigi.cxx
@@ -25,10 +25,10 @@ void strawtubesDigi::driftTimeCalculation(Double_t dist2Wire) {
 }
 
 void strawtubesDigi::NewDist2WireCalculation(Double_t driftTime, Double_t wireOffset) {
-    parabolaChainsEstimation(wireOffset)
+    parabolaChainsEstimation(wireOffset);
     Double_t checkTime = rightChain->Eval(1.0 - wireOffset);
     if (driftTime < checkTime) {
-       newDist2Wire = rightChain->GetX(driftTime. 0., 4.)
+       newDist2Wire = rightChain->GetX(driftTime, 0., 4.);
     } else {
        newDist2Wire = leftChain->GetX(driftTime, 0., 4.);
     }
