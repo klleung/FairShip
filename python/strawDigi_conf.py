4cbb6e9fede7f4321bacc8e5658fcd7efc015781
Leung, Kwing Lam <kwing.lam.leung@cern.ch>
2019-07-23 13:44:32 +0200

    minor change to prepare the use of shipStrawTracking

--- python/strawDigi_conf.py
+++ python/strawDigi_conf.py
@@ -3,8 +3,8 @@ import shipunit as u
 class StrawtubesMisalign:
     misalign = True
     sameSagging = True
-    maxTubeSagging = 0.7*u.cm
-    maxWireSagging = 0.3*u.cm
+    maxTubeSagging = 0.*u.cm
+    maxWireSagging = 0.*u.cm
     tubeGausSigma = 0.*u.cm
     wireGausSigma = 0.*u.cm
     debug = False
